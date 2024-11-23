9369536a31554a2ebc8d038b0e8295575093d3ba
Edward Esqueda <esquede@outlook.com>
2024-11-22 21:48:54 -0500

    Revert "Merge pull request #3 from esque01/react-fetch-character"
    This reverts commit f2d4d16fc0725d4889f938c082012ab09b2c5acd, reversing
    changes made to ab931b83d2a7f818d7c42e974267a9969f2d5d9d.

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,42 +1,9 @@
-import { IconButton, Typography } from '@mui/material';
 import './App.css';
-import CharacterCard from './components/CharacterCard';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import APIClient from './data/APIClient';
-import { useEffect, useState } from 'react';
-import { Character } from './types/Character.interface';
-
 
 function App() {
-  
-  const [character, setCharacter] = useState<Character | undefined>(undefined);
-
-  const fetchCharacter = async() =>  {
-      const randomCharacterId: number = Math.floor(Math.random() * 80) + 1;
-
-      const response = await APIClient.get(`people/${randomCharacterId}`);
-      setCharacter(response.data as Character);
-  }
-
-
-  useEffect(() => {
-    fetchCharacter();
-  }, [])
-
-
-  useEffect(() => { 
-  }, [character]);
-
-
   return (
     <div className="App">
-        <div style={{ margin: "25px", textAlign: 'center' }}>
-          <Typography>Fetch Character</Typography>
-          <IconButton onClick={fetchCharacter}>
-            <RefreshIcon />
-          </IconButton>
-        </div>
-      <CharacterCard character={character}></CharacterCard>
+      
     </div>
   );
 }
